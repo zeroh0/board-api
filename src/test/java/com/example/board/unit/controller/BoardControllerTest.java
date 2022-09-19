9eea3e29913af189d1d61e29bf06d608ba70726d
zeroh0 <umberto1900@naver.com>
2022-09-19 22:44:50 +0900

    test: 게시글 수정 API 테스트 (controller)

--- src/test/java/com/example/board/unit/controller/BoardControllerTest.java
+++ src/test/java/com/example/board/unit/controller/BoardControllerTest.java
@@ -1,9 +1,7 @@
 package com.example.board.unit.controller;
 
 import com.example.board.controller.BoardController;
-import com.example.board.dto.BoardResponse;
-import com.example.board.dto.CreateBoardRequest;
-import com.example.board.dto.CreateBoardResponse;
+import com.example.board.dto.*;
 import com.example.board.service.BoardService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -45,6 +43,22 @@ class BoardControllerTest {
 
 		// when, then
 		Assertions.assertThat(boardController.save(requestDto).getBody().getId()).isEqualTo(1L);
+	}
+
+	@Test
+	@DisplayName("게시글 수정 테스트")
+	void update() {
+		// given
+		UpdateBoardRequest requestDto = UpdateBoardRequest.builder()
+			.id(1L)
+			.title("게시글 제목 수정")
+			.cont("게시글 내용 수정")
+			.build();
+
+		when(boardService.update(requestDto)).thenReturn(UpdateBoardResponse.builder().id(1L).build());
+
+		// when, then
+		Assertions.assertThat(boardController.update(requestDto).getBody().getId()).isEqualTo(1L);
 	}
 
 	@Test
