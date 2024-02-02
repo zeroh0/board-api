ba9ee532a8f6dd610ed07d80dcda663325409bc8
zeroh0 <umberto1900@naver.com>
2024-02-02 21:49:45 +0900

    Chore: .adoc 문자 UTF-8 인코딩
    한글 깨짐 방지

--- src/test/java/com/example/board/docs/RestDocsTemplate.java
+++ src/test/java/com/example/board/docs/RestDocsTemplate.java
@@ -6,6 +6,7 @@ import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
@@ -18,6 +19,7 @@ public abstract class RestDocsTemplate {
 	void setUp(RestDocumentationContextProvider restDocumentation) {
 		this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
 				.apply(documentationConfiguration(restDocumentation))
+				.addFilters(new CharacterEncodingFilter("UTF-8", true))
 				.build();
 	}
 
